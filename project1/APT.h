e4218396326832abdef7e05cba896e812f5f44b6
lightsing <light.tsing@gmail.com>
2019-09-21 18:22:46 +0800

    Create Annotated Parse Tree Header

---
+++ project1/APT.h
@@ -0,0 +1,28 @@
+/*
+ * Annotated Parse Tree
+ */
+#ifndef _LT_APT_H
+#define _LT_APT_H
+
+typedef struct AnnotatedParseTreeNode {
+    int lineno;
+    char* name;
+
+    struct AnnotatedParseTreeNode* left;
+    struct AnnotatedParseTreeNode* right;
+
+    // Node value
+    union
+    {
+        int int_value;
+        float float_value;
+        char* string_value;
+    };
+    
+} AnnotatedParseTreeNode;
+
+AnnotatedParseTreeNode* newAnnotatedParseNode(char* name, int cnt, ...);
+
+AnnotatedParseTreeNode* newAnnotatedParseLeafNode(char* name, int line);
+
+#endif
